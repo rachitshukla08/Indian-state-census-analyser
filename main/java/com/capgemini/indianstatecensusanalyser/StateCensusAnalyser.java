246c5819299ad604abc23609e8bb926ee29cba4a
unknown <rachitshukla08@gmail.com>
2020-10-17 15:56:34 +0530

    Added: TC1.4 to throw InvalidDelimiter exception type when invalid delimiter is used

--- main/java/com/capgemini/indianstatecensusanalyser/StateCensusAnalyser.java
+++ main/java/com/capgemini/indianstatecensusanalyser/StateCensusAnalyser.java
@@ -3,6 +3,8 @@
  */
 package com.capgemini.indianstatecensusanalyser;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -10,6 +12,8 @@ import java.nio.file.Paths;
 import java.util.Iterator;
 
 import com.capgemini.indianstatecensusanalyser.customexception.CensusAnalyserException;
+import com.capgemini.indianstatecensusanalyser.customexception.CensusAnalyserException.ExceptionType;
+import com.capgemini.indianstatecensusanalyser.model.IndiaStateCensus;
 import com.capgemini.indianstatecensusanalyser.model.IndiaStateCensus;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -23,7 +27,7 @@ public class StateCensusAnalyser {
 	/**
 	 * @param censusDataPath
 	 * @return number of entries
-	 * @throws CensusAnalyserException 
+	 * @throws CensusAnalyserException
 	 */
 	public int loadCensusData(String censusDataPath) throws CensusAnalyserException {
 		try {
@@ -32,15 +36,28 @@ public class StateCensusAnalyser {
 			csvToBeanBuilder.withType(IndiaStateCensus.class);
 			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
 			CsvToBean<IndiaStateCensus> csvToBean = csvToBeanBuilder.build();
+			
+			BufferedReader br = new BufferedReader(new FileReader(censusDataPath)); 
+			String line = "";
+	            while ((line = br.readLine()) != null) {
+	            	if(!line.contains(","))
+	            		throw new CensusAnalyserException("Invalid delimiter", ExceptionType.INVALID_DELIMITER);
+	            }
+			br.close();
+			
 			Iterator<IndiaStateCensus> censusIterator = csvToBean.iterator();
 			int noOfEntries = 0;
 			while (censusIterator.hasNext()) {
 				noOfEntries++;
 				IndiaStateCensus censusData = censusIterator.next();
+				System.out.println(censusData);
 			}
 			return noOfEntries;
 		} catch (IOException e) {
-			throw new CensusAnalyserException("Invalid file location");
+			throw new CensusAnalyserException("Invalid file location", ExceptionType.INVALID_FILE_PATH);
+		} catch (IllegalStateException e) {
+			throw new CensusAnalyserException("Incorrect class type", ExceptionType.INVALID_CLASS_TYPE);
 		}
+
 	}
 }
